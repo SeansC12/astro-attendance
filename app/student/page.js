d95ce36319812079c12db7d90b6f2168ef6a6246
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2023-09-16 11:39:01 +0800

    new fancy modal

--- app/student/page.js
+++ app/student/page.js
@@ -5,6 +5,19 @@ import { QrScanner } from "@yudiel/react-qr-scanner";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
 function page() {
   async function scanned(result) {
     const confirmed = window.confirm(
@@ -82,19 +95,68 @@ function page() {
   }
 
   const [text, setText] = useState("");
+  const [open, setOpen] = useState("");
+  const [qrResult, setQrResult] = useState("");
   const supabase = createClientComponentClient();
 
   return (
     <div className="w-[100vw] h-[100vh] flex items-center justify-center">
       <div className="w-[80%] aspect-square">
-        <QrScanner onDecode={(result) => scanned(result)} />
+        <QrScanner
+          onDecode={(result) => setQrResult(result)}
+        />
         <div>{text}</div>
         <Link href="/student/absentee-form">
           <p>Absentee Form</p>
         </Link>
       </div>
-      <button onClick={fake_scanned}>test</button>
+      <button onClick={() => setOpen((curr) => !curr)}>
+        open
+      </button>
+      <AlertDialogDemo
+        open={open}
+        setOpen={setOpen}
+        callback={fake_scanned}
+        result={qrResult}
+      />
     </div>
+  );
+}
+
+function AlertDialogDemo({
+  open,
+  setOpen,
+  callback,
+  result,
+}) {
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        {/* <Button variant="outline">Show Dialog</Button> */}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-white">
+            Would you like to log your attendance?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Please do not leave this page until a Success
+            page has been shown. Attendance may not be
+            logged should you leave the page prematurely.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel className="text-white">
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => callback(result)}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
